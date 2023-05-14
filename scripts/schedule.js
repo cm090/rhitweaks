54738dbe5017440a5052c12a11f22086936670a7
Canon <cm090.tech@gmail.com>
2023-05-14 11:42:46 -0400

    Migrate remote schedule lookup code to local

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -18,16 +18,16 @@ const runApp = allowPrint => {
 	printListener();
 	if (document.forms[0] && document.querySelector("body > form > table > tbody > tr:nth-child(4) > td:nth-child(1)") && document.querySelector("body > form > table > tbody > tr:nth-child(4) > td:nth-child(1)").innerText.includes('ID')) {
 		const quarters = document.forms[0].termcode.innerHTML;
-		fetch('https://raw.githubusercontent.com/cm090/rhit-moodle-tweaks/main/assets/courselookup-home').then(res => res.text().then(data => {
+		fetch(chrome.runtime.getURL('assets/schedule/home.html')).then(res => res.text().then(data => {
 			document.querySelector('html').innerHTML = data;
 			document.querySelector('#quarters').innerHTML = quarters;
 		}));
 	} else if (document.title.includes('Course Matrix View'))
-		fetch('https://raw.githubusercontent.com/cm090/rhit-moodle-tweaks/main/assets/courselookup-matrix').then(res => res.text()).then(data => document.querySelector('body').innerHTML = data + document.querySelector('body').innerHTML);
+		fetch(chrome.runtime.getURL('assets/schedule/matrix.html')).then(res => res.text()).then(data => document.querySelector('body').innerHTML = data + document.querySelector('body').innerHTML);
 	else if (document.querySelectorAll('table').length > 1)
-		fetch('https://raw.githubusercontent.com/cm090/rhit-moodle-tweaks/main/assets/courselookup-detail').then(res => res.text()).then(data => document.querySelector('body').innerHTML = data + document.querySelector('body').innerHTML);
+		fetch(chrome.runtime.getURL('assets/schedule/detail.html')).then(res => res.text()).then(data => document.querySelector('body').innerHTML = data + document.querySelector('body').innerHTML);
 	else if (document.title.includes('Schedule Options'))
-		fetch('https://raw.githubusercontent.com/cm090/rhit-moodle-tweaks/main/assets/courselookup-schedule').then(res => res.text()).then(data => document.querySelector('body').innerHTML = data + document.querySelector('body').innerHTML);
+		fetch(chrome.runtime.getURL('assets/schedule/lookup.html')).then(res => res.text()).then(data => document.querySelector('body').innerHTML = data + document.querySelector('body').innerHTML);
 }
 
 chrome.storage.sync.get('schedule').then(data => {
