9d074f585649ab416bfec7df25a253d337139512
Canon <22649817+cm090@users.noreply.github.com>
2024-05-14 18:23:08 -0400

    Toggling tweaks controls setting button display

--- src/pages/popup/components/App.tsx
+++ src/pages/popup/components/App.tsx
@@ -1,19 +1,17 @@
-import { CssVarsProvider, Sheet, Typography } from '@mui/joy';
+import { Sheet, Typography } from '@mui/joy';
 import React from 'react';
 import ToggleItem from './ToggleItem';
 
 const App = (): JSX.Element => {
   return (
-    <CssVarsProvider defaultMode="system">
-      <Sheet>
-        <Typography level="h1" textAlign="center">
-          RHITweaks
-        </Typography>
-        <ToggleItem name="Moodle" />
-        <ToggleItem name="Schedule lookup" />
-        <ToggleItem name="Banner web" />
-      </Sheet>
-    </CssVarsProvider>
+    <Sheet>
+      <Typography level="h2" textAlign="center">
+        RHITweaks
+      </Typography>
+      <ToggleItem name="Moodle" />
+      <ToggleItem name="Schedule lookup" />
+      <ToggleItem name="Banner web" />
+    </Sheet>
   );
 };
 
--- src/pages/popup/components/ToggleItem.tsx
+++ src/pages/popup/components/ToggleItem.tsx
@@ -1,18 +1,33 @@
 import { Settings } from '@mui/icons-material';
 import { Box, Checkbox, IconButton } from '@mui/joy';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface ToggleItemProps {
   name: string;
 }
 
 const ToggleItem = (props: ToggleItemProps): JSX.Element => {
+  const [enabled, setEnabled] = useState<boolean>(false);
+
   return (
-    <Box>
-      <Checkbox variant="outlined" label={`${props.name} tweaks`} />
-      <IconButton variant="solid" color="primary" size="sm">
-        <Settings />
-      </IconButton>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '40px',
+      }}
+    >
+      <Checkbox
+        variant="solid"
+        label={`${props.name} tweaks`}
+        onChange={(e) => setEnabled(e.target.checked)}
+      />
+      {enabled && (
+        <IconButton variant="solid" color="primary" size="sm">
+          <Settings />
+        </IconButton>
+      )}
     </Box>
   );
 };
