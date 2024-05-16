4a60df6f62fded5e0ddb30f70cfb80cf9b2487cf
Canon <22649817+cm090@users.noreply.github.com>
2024-05-16 16:39:11 -0400

    Update CSS conditional loading

--- src/content/moodle/moodle.ts
+++ src/content/moodle/moodle.ts
@@ -4,15 +4,40 @@ import { moodleDefaults } from '../../defaults';
 import { MoodleData } from '../../types';
 import headerButtons from './header-buttons.html';
 import searchModal from './search-modal.html';
-import styles from './styles.css';
 
 const courseData = [['Dashboard', 'https://moodle.rose-hulman.edu/my']];
 let moodleData: MoodleData = {} as MoodleData;
 
 const setStyle = async () => {
-  const s = document.createElement('style');
-  s.textContent = styles;
-  document.getElementsByTagName('head')[0].appendChild(s);
+  import('./styles.css').then(() => {
+    document
+      .querySelectorAll('style,link[rel="stylesheet"]')
+      .forEach((sheet) => {
+        if (sheet) {
+          try {
+            const rules =
+              (sheet as HTMLStyleElement).sheet!.cssRules ||
+              (sheet as HTMLStyleElement).sheet!.rules;
+            for (let i = 0; i < rules.length; i++) {
+              const rule = rules[i];
+              if (
+                rule.cssText.includes('maroon') ||
+                rule.cssText.includes('rgb(128, 0, 0)')
+              ) {
+                const newRule = rule.cssText.replace(
+                  /maroon|rgb\(128, 0, 0\)/g,
+                  '--accent-color',
+                );
+                (sheet as HTMLStyleElement).sheet!.deleteRule(i);
+                (sheet as HTMLStyleElement).sheet!.insertRule(newRule, i);
+              }
+            }
+          } catch (e) {
+            // Ignore
+          }
+        }
+      });
+  });
   if (document.querySelector('.gradeparent')) {
     const wait = () => {
       const shortcuts = document.querySelector('#available_shortcuts_popup');
@@ -29,31 +54,6 @@ const setStyle = async () => {
     };
     wait();
   }
-  document.querySelectorAll('style,link[rel="stylesheet"]').forEach((sheet) => {
-    if (sheet) {
-      try {
-        const rules =
-          (sheet as HTMLStyleElement).sheet!.cssRules ||
-          (sheet as HTMLStyleElement).sheet!.rules;
-        for (let i = 0; i < rules.length; i++) {
-          const rule = rules[i];
-          if (
-            rule.cssText.includes('maroon') ||
-            rule.cssText.includes('rgb(128, 0, 0)')
-          ) {
-            const newRule = rule.cssText.replace(
-              /maroon|rgb\(128, 0, 0\)/g,
-              '--accent-color',
-            );
-            (sheet as HTMLStyleElement).sheet!.deleteRule(i);
-            (sheet as HTMLStyleElement).sheet!.insertRule(newRule, i);
-          }
-        }
-      } catch (e) {
-        // Ignore
-      }
-    }
-  });
   return await Promise.resolve();
 };
 
