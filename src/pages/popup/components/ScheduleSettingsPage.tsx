22479da408632bff7f26cbce6d64916330c9f1a6
Canon <22649817+cm090@users.noreply.github.com>
2024-06-22 10:05:21 -0500

    Add color pickers to schedule settings page

--- src/pages/popup/components/ScheduleSettingsPage.tsx
+++ src/pages/popup/components/ScheduleSettingsPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { scheduleDefaults } from '../../../defaults';
 import { Page, ScheduleData } from '../../../types';
+import ColorPicker from './ColorPicker';
 import SettingsWrapper from './SettingsWrapper';
 
 interface ScheduleSettingsPageProps {
@@ -13,7 +15,38 @@ const ScheduleSettingsPage = (
 ): JSX.Element => {
   return (
     <SettingsWrapper page="Schedule Lookup" back={() => props.setPage('home')}>
-      <div>Coming soon</div>
+      <ColorPicker
+        label="Background color"
+        defaultValue={scheduleDefaults.bgColor}
+        data={props.data.bgColor}
+        setData={(bgColor) =>
+          props.setData((prevData) => ({ ...prevData, bgColor }))
+        }
+      />
+      <ColorPicker
+        label="Accent color"
+        defaultValue={scheduleDefaults.accentColor}
+        data={props.data.accentColor}
+        setData={(accentColor) =>
+          props.setData((prevData) => ({ ...prevData, accentColor }))
+        }
+      />
+      <ColorPicker
+        label="Text color"
+        defaultValue={scheduleDefaults.textColor}
+        data={props.data.textColor}
+        setData={(textColor) =>
+          props.setData((prevData) => ({ ...prevData, textColor }))
+        }
+      />
+      <ColorPicker
+        label="Border color"
+        defaultValue={scheduleDefaults.borderColor}
+        data={props.data.borderColor}
+        setData={(borderColor) =>
+          props.setData((prevData) => ({ ...prevData, borderColor }))
+        }
+      />
     </SettingsWrapper>
   );
 };
