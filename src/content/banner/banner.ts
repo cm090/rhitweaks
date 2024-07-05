fa7db9fb6f3b0ddc4683e58b8b21ebe8740ae5c3
Canon <22649817+cm090@users.noreply.github.com>
2024-07-05 11:38:06 -0500

    Refactor Banner load script

--- src/content/banner/banner.ts
+++ src/content/banner/banner.ts
@@ -52,71 +52,9 @@ const buildLinks = (links: BannerLink[]) => {
 };
 
 const runApp = (links: BannerData['links']) => {
-  const linkData = links === 'student' ? studentLinks : facultyLinks;
   try {
-    if (window.location.href.includes('BannerGeneralSsb/ssb/general#/home')) {
-      const load = () => {
-        if (!document.querySelector('.loading')) {
-          const mainView = document.querySelector('.gen-home-main-view');
-          if (mainView) {
-            mainView.innerHTML = homePage;
-          }
-          setTimeout(() => buildLinks(linkData), 100);
-        } else {
-          setTimeout(load, 100);
-        }
-      };
-      load();
-    } else if (window.location.href.includes('logout/timeoutPage')) {
-      window.location.href = 'https://bannerweb.rose-hulman.edu/login';
-    }
-
-    const bannerMenu = document.querySelector('#bannerMenu');
-    if (bannerMenu) {
-      bannerMenu.addEventListener('click', () => {
-        let back = document.querySelector('#backButton') as HTMLButtonElement;
-        while (back) {
-          back.click();
-          back = document.querySelector('#backButton') as HTMLButtonElement;
-        }
-        const bannerList = document.querySelector(
-          '#list_Banner',
-        ) as HTMLButtonElement;
-        if (bannerList) {
-          bannerList.click();
-        }
-        const changeHomepage = () => {
-          const menuList = document.querySelector('#list') as HTMLElement;
-          try {
-            const menuTitle = document.querySelector(
-              '#list span.menu-common',
-            ) as HTMLElement;
-            if (menuList && menuTitle && menuList.innerText === 'Banner') {
-              menuTitle.innerText = 'Home';
-              document
-                .querySelectorAll('#list, #list div.menu-item, #list a')
-                .forEach((item) =>
-                  item.addEventListener('click', () => {
-                    window.location.href =
-                      'https://bannerssb.rose-hulman.edu/BannerGeneralSsb';
-                    const menuContainer =
-                      document.querySelector('#menuContainer');
-                    if (menuContainer) {
-                      menuContainer.classList.remove('show');
-                    }
-                  }),
-                );
-            }
-          } catch {
-            // Ignore
-          }
-          if (menuList.offsetHeight > 0) {
-            setTimeout(changeHomepage, 100);
-          }
-        };
-        changeHomepage();
-      });
-    }
+    addHomepageLinks(links);
+    handleSidebarMenu();
 
     const logo = document.querySelector('#branding') as HTMLAnchorElement;
     logo.href = 'https://bannerssb.rose-hulman.edu/BannerGeneralSsb';
@@ -127,21 +65,97 @@ const runApp = (links: BannerData['links']) => {
   }
 };
 
+const addHomepageLinks = (links: BannerData['links']) => {
+  const linkData = links === 'student' ? studentLinks : facultyLinks;
+  if (window.location.href.includes('BannerGeneralSsb/ssb/general#/home')) {
+    const load = () => {
+      if (!document.querySelector('.loading')) {
+        const mainView = document.querySelector('.gen-home-main-view');
+        if (mainView) {
+          mainView.innerHTML = homePage;
+        }
+        setTimeout(() => buildLinks(linkData), 100);
+      } else {
+        setTimeout(load, 100);
+      }
+    };
+    load();
+  } else if (window.location.href.includes('logout/timeoutPage')) {
+    window.location.href = 'https://bannerweb.rose-hulman.edu/login';
+  }
+};
+
+const handleSidebarMenu = () => {
+  const bannerMenu = document.querySelector('#bannerMenu');
+  if (bannerMenu) {
+    bannerMenu.addEventListener('click', () => {
+      let back = document.querySelector('#backButton') as HTMLButtonElement;
+      while (back) {
+        back.click();
+        back = document.querySelector('#backButton') as HTMLButtonElement;
+      }
+      const bannerList = document.querySelector(
+        '#list_Banner',
+      ) as HTMLButtonElement;
+      if (bannerList) {
+        bannerList.click();
+      }
+      const changeHomepage = () => {
+        const menuList = document.querySelector('#list') as HTMLElement;
+        try {
+          const menuTitle = document.querySelector(
+            '#list span.menu-common',
+          ) as HTMLElement;
+          if (menuList && menuTitle && menuList.innerText === 'Banner') {
+            menuTitle.innerText = 'Home';
+            document
+              .querySelectorAll('#list, #list div.menu-item, #list a')
+              .forEach((item) =>
+                item.addEventListener('click', () => {
+                  window.location.href =
+                    'https://bannerssb.rose-hulman.edu/BannerGeneralSsb';
+                  const menuContainer =
+                    document.querySelector('#menuContainer');
+                  if (menuContainer) {
+                    menuContainer.classList.remove('show');
+                  }
+                }),
+              );
+          }
+        } catch {
+          // Ignore
+        }
+        if (menuList.offsetHeight > 0) {
+          setTimeout(changeHomepage, 100);
+        }
+      };
+      changeHomepage();
+    });
+  }
+};
+
 const getData = () => {
   bannerDataHandler.removeChangeListeners();
-  bannerDataHandler.getDataObject().then((data) => {
-    const bannerData = { ...bannerDefaults, ...data };
-    if (bannerData.enabled) {
-      runApp(bannerData.links);
-    }
-  });
+  bannerDataHandler
+    .getDataObject()
+    .then((data) => {
+      const bannerData = { ...bannerDefaults, ...data };
+      attemptAppStart(bannerData);
+    })
+    .catch(() => attemptAppStart(bannerDefaults));
   bannerDataHandler.onDataChanged((oldData, newData) => {
     if (oldData.enabled != newData.enabled) {
       window.location.reload();
       return;
     }
-    runApp((newData as BannerData).links);
+    attemptAppStart(newData);
   });
+};
+
+const attemptAppStart = (data: BannerData) => {
+  if (data.enabled) {
+    runApp(data.links);
+  }
 };
 
 getData();
