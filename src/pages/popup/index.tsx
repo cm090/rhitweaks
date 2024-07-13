ab1c349b85c41b795a138b830c2ff1402b5141bd
Canon <22649817+cm090@users.noreply.github.com>
2024-07-13 10:20:10 -0500

    Add Firefox permission request

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ThemeHandler from './components/ThemeHandler';
 
+const requestHostPermissions = () => {
+  chrome.permissions.request({
+    origins: [
+      'https://moodle.rose-hulman.edu/*',
+      'https://prodwebxe-hv.rose-hulman.edu/*',
+      'https://prodwebxe7-hv.rose-hulman.edu/*',
+      'https://bannerssb.rose-hulman.edu/*',
+      'https://print.rhit.cf/*',
+      'https://print.rose-hulman.edu:9192/*',
+    ],
+  });
+};
+
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
 root.render(
@@ -9,3 +22,5 @@ root.render(
     <ThemeHandler />
   </React.StrictMode>,
 );
+
+requestHostPermissions();
