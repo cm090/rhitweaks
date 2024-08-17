907f044ce2ecfee324848600a01188a81f40de33
Canon <22649817+cm090@users.noreply.github.com>
2024-08-17 09:05:44 -0500

    Remove redundant state management

--- src/pages/popup/components/ResetWarningDialog.tsx
+++ src/pages/popup/components/ResetWarningDialog.tsx
@@ -7,7 +7,7 @@ import {
   Modal,
   ModalDialog,
 } from '@mui/joy';
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode } from 'react';
 
 interface ResetWarningDialogProps {
   open: boolean;
@@ -17,43 +17,36 @@ interface ResetWarningDialogProps {
   isAllData?: boolean;
 }
 
-const ResetWarningDialog = (props: ResetWarningDialogProps): ReactNode => {
-  const [open, setOpen] = useState(props.open);
-  const closeDialog = () => {
-    setOpen(false);
-    props.setOpen(false);
-  };
-
-  useEffect(() => setOpen(props.open), [props.open]);
-
-  return (
-    <Modal open={open} onClose={closeDialog}>
-      <ModalDialog variant="outlined" role="alertdialog">
-        <DialogTitle>Reset {props.dataType ?? 'all'} data</DialogTitle>
-        <Divider />
-        <DialogContent>
-          Are you sure you want to reset {props.isAllData ? 'all ' : ''}your{' '}
-          {props.dataType ? `${props.dataType} ` : ''}data? This cannot be
-          undone.
-        </DialogContent>
-        <DialogActions>
-          <Button
-            variant="solid"
-            color="danger"
-            onClick={() => {
-              props.onConfirm();
-              closeDialog();
-            }}
-          >
-            Reset
-          </Button>
-          <Button variant="plain" color="neutral" onClick={closeDialog}>
-            Cancel
-          </Button>
-        </DialogActions>
-      </ModalDialog>
-    </Modal>
-  );
-};
+const ResetWarningDialog = (props: ResetWarningDialogProps): ReactNode => (
+  <Modal open={props.open} onClose={() => props.setOpen(false)}>
+    <ModalDialog variant="outlined" role="alertdialog">
+      <DialogTitle>Reset {props.dataType ?? 'all'} data</DialogTitle>
+      <Divider />
+      <DialogContent>
+        Are you sure you want to reset {props.isAllData ? 'all ' : ''}your{' '}
+        {props.dataType ? `${props.dataType} ` : ''}data? This cannot be undone.
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant="solid"
+          color="danger"
+          onClick={() => {
+            props.onConfirm();
+            props.setOpen(false);
+          }}
+        >
+          Reset
+        </Button>
+        <Button
+          variant="plain"
+          color="neutral"
+          onClick={() => props.setOpen(false)}
+        >
+          Cancel
+        </Button>
+      </DialogActions>
+    </ModalDialog>
+  </Modal>
+);
 
 export default ResetWarningDialog;
