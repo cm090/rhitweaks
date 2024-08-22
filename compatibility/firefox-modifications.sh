94e3e33c58c5fb55f59ab4d199906cd7c9e8afcd
Canon <22649817+cm090@users.noreply.github.com>
2024-08-21 22:32:19 -0500

    Fix firefox script

--- compatibility/firefox-modifications.sh
+++ compatibility/firefox-modifications.sh
@@ -5,7 +5,7 @@ cp compatibility/manifest-firefox.json dist
 cd dist
 
 jq "del(.background)" manifest.json > manifest-temp.json
-../jq.exe -s '.[0] * .[1]' manifest-temp.json manifest-firefox.json > manifest.json
+jq -s '.[0] * .[1]' manifest-temp.json manifest-firefox.json > manifest.json
 rm manifest-*.json
 
 # shellcheck disable=SC2103
